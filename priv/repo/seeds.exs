328bef045b171b74665706ce8d093c2daf4d1be3
Nat Tuck <nat@ferrus.net>
2021-02-24 18:21:43 -0500

    Inject images in seeds.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -12,9 +12,22 @@
 alias PhotoBlog.Repo
 alias PhotoBlog.Users.User
 alias PhotoBlog.Posts.Post
+alias PhotoBlog.Photos
+
+defmodule Inject do
+  def photo(name) do
+    photos = Application.app_dir(:photo_blog, "priv/photos")
+    path = Path.join(photos, name)
+    {:ok, hash} = Photos.save_photo(name, path)
+    hash
+  end
+end
 
 alice = Repo.insert!(%User{name: "alice"})
 bob = Repo.insert!(%User{name: "bob"})
 
-Repo.insert!(%Post{user_id: alice.id, photo_hash: "", body: "Alice says Hi!"})
-Repo.insert!(%Post{user_id: bob.id, photo_hash: "", body: "Bob says garblarg!"})
+elephant = Inject.photo("elephant.jpg")
+moon = Inject.photo("moon.jpg")
+
+Repo.insert!(%Post{user_id: alice.id, photo_hash: elephant, body: "Alice says Hi!"})
+Repo.insert!(%Post{user_id: bob.id, photo_hash: moon, body: "Bob says garblarg!"})
